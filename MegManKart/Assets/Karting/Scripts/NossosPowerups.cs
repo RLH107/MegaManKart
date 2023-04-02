c689c6932dacc7ac4041bd292922cb5aadb01cf3
Nyct1bius <theo.yamada02@gmail.com>
2023-04-01 22:15:17 -0300

    Script da Caixa de Powerup Funcional
    - As caixas de powerup já dão ao player a habilidade de usar um poder aleatório (os poderes em si não estão prontos).
    - Elas ainda não estão animadas na Unity.

---
+++ MegManKart/Assets/Karting/Scripts/NossosPowerups.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NossosPowerups : MonoBehaviour
+{
+    private float randNum = 0;
+
+    public GameObject forceField;
+
+    private bool canUseForceField, canUseBearTrap, canUseZeroBlades, canUseCards, canUseElectricWeb;
+    private bool hasUsedForceField;
+
+    private float forceFieldTimer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        canUseForceField = false;
+        canUseBearTrap = false; 
+        canUseZeroBlades = false;   
+        canUseCards = false;    
+        canUseElectricWeb = false;  
+
+        hasUsedForceField = false;  
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Lootbox") && !canUseBearTrap && !canUseCards && !canUseElectricWeb && !canUseForceField && !canUseZeroBlades)
+        {
+            randNum = Random.Range(1, 6);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (randNum == 1)
+        {
+            canUseForceField = true;
+            Debug.Log("ForceField");
+        }
+        if (randNum == 2)
+        {
+            canUseBearTrap = true;
+            Debug.Log("BearTrap");
+        }
+        if (randNum == 3)
+        {
+            canUseZeroBlades = true;
+            Debug.Log("ZeroBlades");
+        }
+        if (randNum == 4)
+        {
+            canUseElectricWeb = true;
+            Debug.Log("ElectricWeb");
+        }
+        if (randNum == 5)
+        {
+            canUseCards = true;
+            Debug.Log("Cards");
+        }
+
+        if(hasUsedForceField)
+        {
+            forceFieldTimer -= Time.deltaTime;
+
+            if (forceFieldTimer <= 0)
+            {
+                forceField.SetActive(false);
+                hasUsedForceField = false;
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (canUseElectricWeb)
+        {
+            Debug.Log("UsedElectricWeb");
+            canUseElectricWeb = false;
+            randNum = 0;
+        }
+        if (canUseBearTrap)
+        {
+            Debug.Log("UsedBearTrap");
+            canUseBearTrap = false;
+            randNum = 0;
+        }
+        if (canUseCards)
+        {
+            Debug.Log("UsedCards");
+            canUseCards = false;
+            randNum = 0;
+        }
+        if (canUseForceField)
+        {
+            Debug.Log("UsedForceField");
+            forceField.SetActive(true);
+            canUseForceField = false;
+            hasUsedForceField = true;
+            randNum = 0;
+        }
+        if (canUseZeroBlades)
+        {
+            Debug.Log("UsedZeroBlades");
+            canUseZeroBlades = false;
+            randNum = 0;
+        }
+    }
+}
